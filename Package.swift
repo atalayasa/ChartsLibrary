11e50a3a1b91ffa6bce23c16d1b089f3324419c0
atalayasa <atalayasa94@gmail.com>
2023-04-05 11:28:20 +0300

    changed package name

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "Charts",
+            name: "ChartsLibrary",
             targets: ["Charts"]),
         .library(
             name: "ChartsDynamic",
